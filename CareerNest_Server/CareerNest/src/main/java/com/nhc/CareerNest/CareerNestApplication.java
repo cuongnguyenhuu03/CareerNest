268c5dafef8b4dc0febd05106a839a07d5f522f6
CuongDepTrai <nguyenhuucuong111103@gmail.com>
2024-12-31 09:14:59 +0700

    1st commit

---
+++ CareerNest_Server/CareerNest/src/main/java/com/nhc/CareerNest/CareerNestApplication.java
@@ -0,0 +1,14 @@
+package com.nhc.CareerNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+public class CareerNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerNestApplication.class, args);
+	}
+
+}
